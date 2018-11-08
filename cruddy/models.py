8873cf306fd6c8c0101f34b49ee4ad2b841e12dc
petrosky <1aIIbXXufd77ceKCyktU>
2018-11-09 09:45:23 +1100

    Commit from work

--- cruddy/models.py
+++ cruddy/models.py
@@ -8,14 +8,22 @@ class Users(models.Model):
     last_name = models.CharField(max_length=30)
     user_level = models.IntegerField()
 
+    def __str__(self):
+        return self.first_name + ' ' + self.last_name
 
-class Tasks(models.Model):
+
+class Task(models.Model):
     user = models.ForeignKey(Users, on_delete=models.CASCADE)
     task_name = models.CharField(max_length=150)
     due_date = models.DateTimeField()
 
+    def __str__(self):
+        return self.task_name
+
 
 class TaskList(models.Model):
-    task = models.ForeignKey(Tasks, on_delete=models.CASCADE)
+    task = models.ForeignKey(Task, on_delete=models.CASCADE)
     task_list = models.CharField(max_length=250)
 
+    def __str__(self):
+        return self.task_list
