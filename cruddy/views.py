aacb172821d814a6265a15b4faf88bb38ea9e9db
petrosky <1aIIbXXufd77ceKCyktU>
2018-11-09 13:48:05 +1100

    Commit from work

--- cruddy/views.py
+++ cruddy/views.py
@@ -2,7 +2,14 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.http import HttpResponse
+from .models import Task
+from django.template import loader
 
 
 def index(request):
-    return HttpResponse("This is the app landing page.")
+    tasks = Task.objects.order_by('due_date')[:5]
+    template = loader.get_template('cruddy/index.html')
+    context = {
+        'tasks': tasks,
+    }
+    return HttpResponse(template.render(context, request))
